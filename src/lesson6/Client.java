c0c5b227be17872ec056baa5e32e222b096e9cd0
andrew <359747@mail.ru>
2019-03-03 05:41:32 +0700

    Домашнее задание 6

---
+++ src/lesson6/Client.java
@@ -0,0 +1,57 @@
+package lesson6;
+
+import java.io.*;
+import java.net.*;
+
+class Client {
+    private Socket socket;
+    BufferedReader in;
+    private PrintWriter out;
+    private BufferedReader console;
+
+    public Client() throws IOException {
+        socket = new Socket("localhost",1234);
+        in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(),true);
+        console = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    /**
+     * Отправляем сообщение на сервер. Закрываем приложение по ключевым словам
+     * @throws IOException
+     */
+    void sendToServer() throws IOException {
+        String userMessage;
+
+        while (true) {
+            if ((userMessage = console.readLine()) != null) {
+                out.println(userMessage);
+
+                if (userMessage.equalsIgnoreCase("exit") || userMessage.equalsIgnoreCase("quit")) {
+                    System.out.println("\n === Exiting... ===");
+                    close();
+                }
+            }
+        }
+    }
+
+    /**
+     * Выводим в консоль клиента сообщение сервера
+     * @param msg - сообщение
+     */
+    void writeToConsole(String msg) {
+        System.out.println("lesson6.Server: " + msg);
+    }
+
+    /**
+     * Закрываем приложение
+     * @throws IOException
+     */
+    void close() throws IOException {
+        out.close();
+        in.close();
+        console.close();
+        socket.close();
+        System.exit(0);
+    }
+}
---
+++ src/lesson6/Server.java
@@ -0,0 +1,73 @@
+package lesson6;
+
+import java.io.*;
+import java.net.*;
+
+class Server {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    BufferedReader in;
+    private PrintWriter out;
+
+    public Server()
+    {
+        try {
+            serverSocket = new ServerSocket(1234);
+        } catch (IOException e) {
+            System.out.println("Can't open port 1234");
+            System.exit(1);
+        }
+        System.out.println("=== lesson6.Server started. Waiting for a client... ===");
+
+        try {
+            socket = serverSocket.accept();
+            System.out.println("=== lesson6.Client connected ===");
+
+            in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(),true);
+        } catch (IOException e) {
+            System.out.println("=== Some problems during accepting client ===");
+            System.exit(1);
+        }
+
+        System.out.println("=== Wait for messages... === \n");
+    }
+
+    /**
+     * Отправляем сообщение клиенту. Закрываем приложение по ключевым словам
+     * @throws IOException
+     */
+    void sendToClient() throws IOException, InterruptedException {
+        while (true) {
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            String msg = console.readLine();
+            out.println(msg);
+
+            if (msg.equalsIgnoreCase("exit") || msg.equalsIgnoreCase("quit")) {
+                System.out.println("\n === Exiting... ===");
+                Thread.sleep(2000);
+                close();
+            }
+        }
+    }
+
+    /**
+     * Выводим в консоль сервера сообщение клиента
+     * @param msg - сообщение
+     */
+    void writeToConsole(String msg) {
+        System.out.println("lesson6.Client: " + msg);
+    }
+
+    /**
+     * Закрываем приложение
+     * @throws IOException
+     */
+    void close() throws IOException {
+        out.close();
+        in.close();
+        socket.close();
+        serverSocket.close();
+        System.exit(0);
+    }
+}
